5496c25db34156b26819aefd048d884de85bc55c
log2-2 <ennichi824@gmail.com>
2023-10-29 13:17:31 +0900

    チェックボックスの値を変更するとp要素の背景色が変わるようにした。

--- src/scripts/station13-common.js
+++ src/scripts/station13-common.js
@@ -1,2 +1,12 @@
 const check = document.getElementById("check");
-check.addEventListener("change", changeBackgroundColor);
+// addEventListenerは、クリックやキー入力などの様々なイベント処理を
+// 実行するメソッド
+// 第１引数にイベントの種類、第２引数に関数を指定する
+// 第１引数に指定できる値には、click、keydown、change、submitなどがある
+// changeを指定した場合、ユーザがinput要素やselect要素やtextarea要素の値を
+// 変更したときに実行される。
+check.addEventListener("change", () => changeBackgroundColor(check));
+// check.addEventListener("change", changeBackgroundColor(check));
+// だと即時に呼び出されてしまう。
+// 第２引数に関数の呼び出しではなく関数の参照を渡す必要がある。
+// 関数の参照を渡すには、アロー関数を用いる。
--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
-function changeBackgroundColor(e) {
+function changeBackgroundColor(check) {
   // ここに背景色を変える処理を書く。
-
+  const textElement = document.getElementById("text");
+  if (check.checked) {
+    textElement.style.backgroundColor = "red";
+  } else {
+    // 背景色を無色にするにはbackgroun-colorプロパティの値をtransparentとする
+    textElement.style.backgroundColor = "transparent";
+  }
 }
